7f9f8eaefbbc2b255ec3e487b6810c720dfda8e9
yeerang <leealyoo@gmail.com>
2022-04-14 22:34:13 +0900

    no message

---
+++ ch03/ch03_1/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {StyleSheet, SafeAreaView, Text} from 'react-native'
+import {Colors} from 'react-native-paper'
+import Color from 'color'
+
+console.log(Colors.blue500)
+console.log(Color(Colors.blue500).alpha(0.5).lighten(0.5).string())
+
+export default function App() {
+  return (
+    <SafeAreaView style={[styles.safeAreaView]}>
+      <Text style={[styles.text]}>Hello World!</Text>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  safeAreaView:{flex:1, alignItems:'center', justifyContent:'center', backgroundColor:Colors.blue500},
+  text:{fontSize:20, color:Color(Colors.blue500).alpha(0.7).lighten(0.9).string()}
+})
