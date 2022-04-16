511c8d9073f7b50d8957e29ec12759a12a62a28f
ririyu <leealyoo@gmail.com>
2022-04-16 10:19:48 +0900

    react

---
+++ ch04/ch04_2/src/screens/Cache.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {StyleSheet, View, Text} from 'react-native'
+import {Colors} from 'react-native-paper'
+import * as D from '../data'
+
+const people = D.makeArray(2).map(D.createRandomPerson)
+const title = 'Cache'
+export default function Cache() {
+  return (
+    <View style={[styles.view]}>
+      <Text style={[styles.text]}>{title}</Text>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  view: {flex:1, padding: 5, backgroundColor: Colors.blue900},
+  text: {fontSize: 20, color: 'white'}
+})
